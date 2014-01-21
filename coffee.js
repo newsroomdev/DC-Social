e4f7461ad7e3a2d9abaf5d03574b2b364a342233
geraldarthur <gerald.a.rich@gmail.com>
2014-01-20 23:48:05 -0500

    initial commit

---
+++ coffee.js
@@ -0,0 +1,245 @@
+var cafes = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "The Coffee Bar",
+        "login": "drinkmorecoffee",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.02827,
+          38.91427
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Bistro Bohem",
+        "login": "2027355895",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.02013,
+          38.91538
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Black Cat",
+        "login": "luckycat",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.03155,
+          38.91458
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Snap",
+        "login": "nutella1",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.04227,
+          38.92239
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Columbia Heights Coffee",
+        "login": "FAIRTRADE1",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.02854,
+          38.93222
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Azi's Cafe",
+        "login": "sunny",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.02419,
+          38.90842
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Blind Dog Cafe",
+        "login": "baxtercantsee",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.02518,
+          38.91931
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Le Caprice",
+        "login": "baguette",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.03304,
+          38.9326
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Filter",
+        "login": "",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.04509,
+          38.91368
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Peregrine",
+        "login": "espresso",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.99656,
+          38.88516
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Tryst",
+        "login": "coupetnt",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.042438,
+          38.921894
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "The Coupe",
+        "login": "voteforus",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.02821,
+          38.93206
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Big Bear Cafe",
+        "login": "",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.01239,
+          38.91275
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Sidamo",
+        "login": "",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.9999,
+          38.90006
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "place": "Chinatown Coffee",
+        "login": "",
+        "marker-color": "#5D3C06",
+        "marker-symbol": "cafe"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.01863944530487,
+          38.89996124854683
+        ]
+      }
+    }
+  ]
+};
